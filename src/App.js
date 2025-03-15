5727e77d21f7902bfa5bdf3c7860958b446cdf79
sanjana231099 <savash@iu.edu>
2025-03-15 04:06:53 -0400

    updated code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import QuizCreator from './components/QuizCreator';
 import TakeQuiz from './components/TakeQuiz';
